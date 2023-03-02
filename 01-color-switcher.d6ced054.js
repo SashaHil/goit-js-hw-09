b25a3594ce337a0fa6c285769a7e5ba6511c6f35
SashaHil <51095721+SashaHil@users.noreply.github.com>
2023-03-02 17:13:06 +0000

    Deploying to gh-pages from @ SashaHil/goit-js-hw-09@3c96e82409e59460f5201b3eb1936388ec28fa42 🚀

---
+++ 01-color-switcher.d6ced054.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),closeBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.closeBtn.disabled=!0;t.startBtn.addEventListener("click",(()=>{id=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.closeBtn.disabled=!1})),t.closeBtn.addEventListener("click",(()=>{clearInterval(id),t.closeBtn.disabled=!0,t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.d6ced054.js.map
